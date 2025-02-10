f93e9a1afa323a6ac8a6c3a4c67fca65353263b4
holidayfanboy <mkdaniel2020@gmail.com>
2025-02-10 13:53:08 +0900

    castanimation
    1

--- Assets/Daniel/Scripts/PlayerAnimation.cs
+++ Assets/Daniel/Scripts/PlayerAnimation.cs
@@ -7,6 +7,10 @@ public class PlayerAnimation : MonoBehaviour
     private SpriteRenderer playerSpriteRenderer;
     private Animator playeranimator;
     public bool isCrouched = false;
+    [SerializeField] bool isHurt = false;
+    [SerializeField] bool isDamaged = false;
+    float moveX = 0f;
+    
     void Awake()
     {
         playerSpriteRenderer = GetComponent<SpriteRenderer>();  
@@ -20,11 +24,11 @@ public class PlayerAnimation : MonoBehaviour
         {
         
             playerSpriteRenderer.flipX = false;
-            
+            moveX = +1f;
         }
         else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-        
+            moveX = -1f;
             playerSpriteRenderer.flipX = true;     
         }
 
@@ -34,10 +38,45 @@ public class PlayerAnimation : MonoBehaviour
             isCrouched = true;
             playeranimator.SetBool("Crouched", isCrouched);
         }
-        else if (Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetKeyUp(KeyCode.Space)) //점프
         {
             isCrouched = false;
             playeranimator.SetBool("Crouched", isCrouched);
         }
+
+        Vector3 moveDir = new Vector3(moveX, 0).normalized;
+        transform.position += moveDir * 1 * Time.deltaTime;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("Triggered Hurt");
+            isHurt = true;
+            playeranimator.SetBool("Hurted", isHurt);
+        }
+
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Triggered Ground");
+            CastDeath();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("UnTriggered");
+            isHurt = false;
+            playeranimator.SetBool("Hurted", isHurt);
+        }
+    }
+
+    public void CastDeath()
+    {
+        playeranimator.SetTrigger("DeathCast");
     }
 }
